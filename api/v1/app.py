699cae83aa6cb136708ac0539f9ea7d2b63413c5
Boodenator <boode1000@gmail.com>
2024-08-06 13:39:49 -0700

    app

---
+++ api/v1/app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+App concept module
+"""
+from os import getenv
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def close_db(obj):
+    """ close() method """
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_foun(error):
+    """ Loads error 404 page not found """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone - RESTful API',
+    'description': 'API created for the hbnb restful project,\
+    Documentation is shown below',
+    'uiversion': 3}
+
+Swagger(app)
+
+if __name__ == "__main__":
+
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
+    app.run(host, int(port), threaded=True)
